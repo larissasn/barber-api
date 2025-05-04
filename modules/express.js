2382778151aa9a15105064bd99990acf93a8e134
larissasn <s4nlari@gmail.com>
2025-05-03 21:21:40 -0300

    feat[barber-api]Utilização do middwers antes de chamar qualquer método. E utilização da biblioteca ejs pra ao inves de retornar JSON, retornar um HTML estilizado.

--- modules/express.js
+++ modules/express.js
@@ -4,9 +4,15 @@ const UserModel = require("../src/models/user.model");
 const app = express();
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.setHeader("content-type", "text/html");
-  res.status(200).send("<h1>hello word</h1>");
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
 });
 
 app.get("/users", async (req, res) => {
